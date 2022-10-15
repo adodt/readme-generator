69557284a05b3fd2c679823a538e24d16ec9ef4b
Gator <allidodt@gmail.com>
2022-10-15 09:23:00 -0700

    commented acceptable criteria into generateMarkdown and started Readme outline.

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -11,8 +11,52 @@ function renderLicenseLink(license) {}
 function renderLicenseSection(license) {}
 
 // TODO: Create a function to generate markdown for README
+
+//title of project
+//description
+//table of contents
+//installation
+//usage
+//license
+//contributing
+//tests
+//questions (guthub username and link to profile. email with instructions on how to reach me)
+//table on contents directs to corresponding section of README
+
+
 function generateMarkdown(data) {
   return `# ${data.title}
+![Github license]
+
+##Description
+${data.description}
+
+##Table of Contents
+-[Installation](#installation)
+-[Usage](#usage)
+-[License](#license)
+-[Contributing](#contributing)
+-[Tests](#tests)
+-[Questions](#questions)
+
+## Installation
+${data.installation}
+
+## Usage
+${data.usage}
+
+## License
+${data.license}
+
+## Contributing
+${data.contributing}
+
+## Tests
+${data.tests}
+
+## Questions
+To view For questions regarding this project, contact me directly at ${data.email}.
+
 
 `;
 }
