e99745770910db1fada9ad870665050bf1cb1cf0
Gator <allidodt@gmail.com>
2022-10-15 10:47:52 -0700

    adding spaces to generateMarkdown

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -32,12 +32,12 @@ function generateMarkdown(data) {
 ${data.description}
 
 ##Table of Contents
--[Installation](#installation)
--[Usage](#usage)
--[License](#license)
--[Contributing](#contributing)
--[Tests](#tests)
--[Questions](#questions)
+-[Installation](#installation)  
+-[Usage](#usage)  
+-[License](#license)  
+-[Contributing](#contributing)  
+-[Tests](#tests)  
+-[Questions](#questions)  
 
 ## Installation
 ${data.installation}
