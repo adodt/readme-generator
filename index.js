b5f0e22a682a76f0ae8cff8bab1bcf568ebad3f7
Gator <allidodt@gmail.com>
2022-10-14 18:34:17 -0700

    connect generateMarkdown.js

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const fs = require('fs')
 const inquirer = require ('inquirer')
+const generatePage = require ('./utils/generateMarkdown.js')
 
 // TODO: Include packages needed for this application
 
